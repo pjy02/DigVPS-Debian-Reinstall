d2f43fb47f89a9992bc179b811104254f79d81e0
Haseo Chen <19926035+bihell@users.noreply.github.com>
2025-09-12 15:12:55 +0800

    Update debian-dd.sh

--- debian-dd.sh
+++ debian-dd.sh
@@ -54,9 +54,58 @@ fi
 echo -ne "\n${aoiBlue}Whether to enable BBR${plain}\n"
 read -p "Please input y/n [Default y]: " enableBBR
 if [[ -z "$enableBBR" || "$enableBBR" =~ ^[Yy]$ ]]; then
+    echo -ne "${aoiBlue}Use advanced (aggressive) TCP tuning?${plain}\n"
+    read -p "y/n [Default n]: " enableBBRAdv
+
+    # Target file inside the installed system
     bbr_path="/etc/sysctl.d/99-sysctl.conf"
+
+    # Minimal safe set (always enabled when BBR is chosen)
+    BBR_MIN_CONTENT="'net.core.default_qdisc = fq' 'net.ipv4.tcp_congestion_control = bbr'"
+
+    # Advanced optional set (toggleable)
+    BBR_ADV_CONTENT="'net.ipv4.tcp_rmem = 8192 262144 536870912' \
+                     'net.ipv4.tcp_wmem = 4096 16384 536870912' \
+                     'net.ipv4.tcp_adv_win_scale = -2' \
+                     'net.ipv4.tcp_collapse_max_bytes = 6291456' \
+                     'net.ipv4.tcp_notsent_lowat = 131072' \
+                     'net.ipv4.ip_local_port_range = 1024 65535' \
+                     'net.core.rmem_max = 536870912' \
+                     'net.core.wmem_max = 536870912' \
+                     'net.core.somaxconn = 32768' \
+                     'net.core.netdev_max_backlog = 32768' \
+                     'net.ipv4.tcp_max_tw_buckets = 65536' \
+                     'net.ipv4.tcp_abort_on_overflow = 1' \
+                     'net.ipv4.tcp_slow_start_after_idle = 0' \
+                     'net.ipv4.tcp_timestamps = 1' \
+                     'net.ipv4.tcp_syncookies = 0' \
+                     'net.ipv4.tcp_syn_retries = 3' \
+                     'net.ipv4.tcp_synack_retries = 3' \
+                     'net.ipv4.tcp_max_syn_backlog = 32768' \
+                     'net.ipv4.tcp_fin_timeout = 15' \
+                     'net.ipv4.tcp_keepalive_intvl = 3' \
+                     'net.ipv4.tcp_keepalive_probes = 5' \
+                     'net.ipv4.tcp_keepalive_time = 600' \
+                     'net.ipv4.tcp_retries1 = 3' \
+                     'net.ipv4.tcp_retries2 = 5' \
+                     'net.ipv4.tcp_no_metrics_save = 1' \
+                     'net.ipv4.ip_forward = 1' \
+                     'fs.file-max = 104857600' \
+                     'fs.inotify.max_user_instances = 8192' \
+                     'fs.nr_open = 1048576'"
+
+    # Build printf args: minimal always, plus advanced if selected
+    if [[ "$enableBBRAdv" =~ ^[Yy]$ ]]; then
+        BBR_CONTENT="$BBR_MIN_CONTENT $BBR_ADV_CONTENT"
+    else
+        BBR_CONTENT="$BBR_MIN_CONTENT"
+    fi
+
+    # Write the content atomically inside target, ensuring directory exists
     target="in-target"
-    BBR="$target sed -i '\$anet.core.default_qdisc = fq' $bbr_path;$target sed -i '\$anet.ipv4.tcp_congestion_control = bbr' $bbr_path;$target sed -i '\$anet.ipv4.tcp_rmem = 8192 262144 536870912' $bbr_path;$target sed -i '\$anet.ipv4.tcp_wmem = 4096 16384 536870912' $bbr_path;$target sed -i '\$anet.ipv4.tcp_adv_win_scale = -2' $bbr_path;$target sed -i '\$anet.ipv4.tcp_collapse_max_bytes = 6291456' $bbr_path;$target sed -i '\$anet.ipv4.tcp_notsent_lowat = 131072' $bbr_path;$target sed -i '\$anet.ipv4.ip_local_port_range = 1024 65535' $bbr_path;$target sed -i '\$anet.core.rmem_max = 536870912' $bbr_path;$target sed -i '\$anet.core.wmem_max = 536870912' $bbr_path;$target sed -i '\$anet.core.somaxconn = 32768' $bbr_path;$target sed -i '\$anet.core.netdev_max_backlog = 32768' $bbr_path;$target sed -i '\$anet.ipv4.tcp_max_tw_buckets = 65536' $bbr_path;$target sed -i '\$anet.ipv4.tcp_abort_on_overflow = 1' $bbr_path;$target sed -i '\$anet.ipv4.tcp_slow_start_after_idle = 0' $bbr_path;$target sed -i '\$anet.ipv4.tcp_timestamps = 1' $bbr_path;$target sed -i '\$anet.ipv4.tcp_syncookies = 0' $bbr_path;$target sed -i '\$anet.ipv4.tcp_syn_retries = 3' $bbr_path;$target sed -i '\$anet.ipv4.tcp_synack_retries = 3' $bbr_path;$target sed -i '\$anet.ipv4.tcp_max_syn_backlog = 32768' $bbr_path;$target sed -i '\$anet.ipv4.tcp_fin_timeout = 15' $bbr_path;$target sed -i '\$anet.ipv4.tcp_keepalive_intvl = 3' $bbr_path;$target sed -i '\$anet.ipv4.tcp_keepalive_probes = 5' $bbr_path;$target sed -i '\$anet.ipv4.tcp_keepalive_time = 600' $bbr_path;$target sed -i '\$anet.ipv4.tcp_retries1 = 3' $bbr_path;$target sed -i '\$anet.ipv4.tcp_retries2 = 5' $bbr_path;$target sed -i '\$anet.ipv4.tcp_no_metrics_save = 1' $bbr_path;$target sed -i '\$anet.ipv4.ip_forward = 1' $bbr_path;$target sed -i '\$afs.file-max = 104857600' $bbr_path;$target sed -i '\$afs.inotify.max_user_instances = 8192' $bbr_path;$target sed -i '\$afs.nr_open = 1048576' $bbr_path;$target sed -i '\$anet.ipv4.tcp_fastopen=3' $bbr_path;"
+    BBR="$target /bin/sh -c \"mkdir -p /etc/sysctl.d; \\
+        printf '%s\\n' $BBR_CONTENT > $bbr_path; \\
+        sysctl --system >/dev/null 2>&1 || true\";"
 else
     BBR=""
 fi
